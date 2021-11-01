39b880ca51383025e2a1373a3bf9d5f6d01a8dc1
nikechervon <kolyachervon@yandex.ru>
2021-11-01 15:25:23 +0500

    Создал необходимые роуты.

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+
+class AuthController extends Controller
+{
+    public function login()
+    {
+        //
+    }
+
+    public function register()
+    {
+        //
+    }
+
+    public function refresh()
+    {
+        //
+    }
+
+    public function reset()
+    {
+        //
+    }
+
+    public function resetCheck()
+    {
+        //
+    }
+
+    public function check()
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,22 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\API\AuthController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+/** Авторизация пользователя */
+Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+/** Регистрация нового пользователя */
+Route::post('/register', [AuthController::class, 'register']);
+
+/** Обновление токена авторизации */
+Route::post('/refresh', [AuthController::class, 'refresh']);
+
+/** Восстановление токена пользователя */
+Route::post('/reset', [AuthController::class, 'reset']);
+
+/** Проверка ссылки на восстановление пароля */
+Route::get('/reset/check', [AuthController::class, 'resetCheck']);
+
+/** Метод проверки авторизации */
+Route::get('/check', [AuthController::class, 'check']);
